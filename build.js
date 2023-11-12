ca7cdf9b9dd834d42c6d57a8f8ed430fbf822175
Coday-meric <contact@aymeric-mai.fr>
2023-11-12 21:54:00 +0100

    Change name and version

--- build.js
+++ build.js
@@ -3,13 +3,13 @@ var electronInstaller = require('electron-winstaller');
 // In this case, we can use relative paths
 var settings = {
     // Specify the folder where the built app is located
-    appDirectory: './dist/aeve-rec-win32-x64',
+    appDirectory: './dist/simcam-win32-x64',
     // Specify the existing folder where
     outputDirectory: './dist/installers',
     // The name of the Author of the app (the name of your company)
     authors: 'Aymeric Maisonneuve',
     // The name of the executable of your built
-    exe: 'Aeve-Rec.exe',
+    exe: 'simcam.exe',
 
     loadingGif: './ajax-loader.gif',
 
@@ -17,9 +17,9 @@ var settings = {
 
     setupIcon: './icon.ico',
 
-    setupExe: 'aeve_rec-setup.exe',
+    setupExe: 'simcam-setup.exe',
 
-    setupMsi: 'aeve_rec-setup.msi'
+    setupMsi: 'simcam-setup.msi'
 
 };
 
